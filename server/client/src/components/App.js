64ac91a7c8566903ec7f0d4717570b8f9e34b830
Shan Zhu <cocakoala1996@gmail.com>
2020-08-01 23:55:01 -0400

    Add exact and newSurvey Route

--- server/client/src/components/App.js
+++ server/client/src/components/App.js
@@ -11,8 +11,10 @@ const App = () => {
         <div>
             <BrowserRouter>
                 <div>
-                    <Route path="/" component={Landing} />
-                    <Route path='/surveys' component={Dashboard} />
+                    <Header />
+                    <Route exact path="/" component={Landing} />
+                    <Route exact path='/surveys' component={Dashboard} />
+                    <Route path='/surveys/new' component={SurveyNew} />
                 </div>
             </BrowserRouter>
         </div>
